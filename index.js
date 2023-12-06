d1d64d748277d7bd7925f2e87663191349851d79
MD Shaharia <adeptshaharia@gmail.com>
2023-12-06 18:03:16 +0600

    Async_await

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-
-  // Promise
-const myPromise=new Promise((resolved,rejected)=>{
-  const user=null;
-  if(!user){
-    rejected('SomeThing Wrong')
-  }else{
-    resolved('Promise Return Succesfully')
+// Promise handaleing method with Node js
+const myPromise = new Promise((resolved, rejected) => {
+  const user = true;
+  if (!user) {
+    rejected("SomeThing Wrong");
+  } else {
+    setTimeout(() => {
+      resolved({ name: "shaharia" });
+      userData.push({ id: 1 });
+    }, 500);
   }
-})
-myPromise.then((res)=>console.log(res)).catch((err)=>console.log(err))
+});
+myPromise
+  .then((res) => console.log("then" + res))
+  .catch((err) => console.log(err));
+
+// maltiful Promise asyn need to promise all function
+const userIds = [1, 2, 3, 4, 5];
+let userData = [];
+for (let i = 0; i < userIds.length; i++) {
+  const userId = userIds[i];
+  userData.push(myPromise);
+}
+Promise.all(userData).then((res) => console.log(res));
+
+// async await
+const promise = new Promise((resoled, rejected) => {
+  setTimeout(() => {
+    resoled("success");
+  }, 100);
+});
+
+async function getData() {
+  const res = await promise;
+  console.log(res);
+}
+getData();
+
+function normal() {
+  promise.then((res) => console.log(res));
+}
+normal();
